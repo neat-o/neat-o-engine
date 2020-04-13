a920adc7c7cdcc9d4db53c7552f926e9db92516f
JM Versteeg <janmerijnversteeg@gmail.com>
2020-04-13 23:18:26 +0200

    add boilerplate stuff based on `yarn create react-app my-app --template typescript`

--- README.md
+++ README.md
@@ -1,1 +1,17 @@
-# advanced stuff goes here
+# NEAT-O-Engine
+
+NEAT-O (a.k.a. NeuroEvolution of Augmenting Topologies-Onderzoekjes)
+
+The aim of this project is to create a modular engine for running cross-platform (browser-based, more specifically) neuro-evolutionary experiments.
+
+### To get started:
+
+```
+npm install
+npm run start
+
+# or if you're using yarn:
+
+yarn
+yarn start
+```
---
+++ src/App.tsx
@@ -0,0 +1,21 @@
+import React from 'react';
+
+function App() {
+    return (
+        <div className="App" style={{
+            width:          '100%',
+            minHeight:      '100vh',
+            background:     'black',
+            color:          'white',
+            fontFamily:     'monospace',
+            display:        'flex',
+            alignItems:     'center',
+            justifyContent: 'center',
+            fontSize:       40,
+        }}>
+            NEAT-O
+        </div>
+    );
+}
+
+export default App;
